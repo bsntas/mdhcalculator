c018c8aca32f54ee11889e93a3787b5d15e1c2f4
Anonymous <temp@gmail.com>
2023-09-23 15:56:00 +0530

    added factorial function by SSS DT.

--- functions.c
+++ functions.c
@@ -9,7 +9,6 @@ int mul(int a, int b){
 }
 int sub(int a, int b){
 	return a-b;
-
 int mul(int a, int b){
 	return a*b;
 }
@@ -17,4 +16,11 @@ int sub(int a, int b)
 {
 	if(a>b)	return a-b;
 	else return b-a;
+int factorial(int num){
+	if(num==1){
+		return 1;
+	}
+	else{
+		return num * factorial(num-1);
+	}
 }
