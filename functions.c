03476b97713fa94965c38918c7ddd9bfd9603c74
Abburu Aharneish <aharneish@gmail.com>
2023-09-23 15:16:57 +0530

    division finction added by Abburu Aharneish and Sairaj Patro

--- functions.c
+++ functions.c
@@ -1,6 +1,13 @@
 int add (int a, int b) {
 	return a+b;
 }
-int mul(int a ,int b){
-	return a*b;
+
+int divide (int a,int b){
+	if b==0{
+		return printf("division not possible");
+	}
+	else{
+		return a/b;
+	}
 }
+
