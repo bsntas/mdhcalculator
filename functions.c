ababec01d33793389b589bd825b59d3d190b7845
unknown <ani.padmanaban2@gmail.com>
2023-09-23 10:16:50 +0530

    ani vivek 2

--- functions.c
+++ functions.c
@@ -1,26 +1,17 @@
 int add (int a, int b) {
 	return a+b;
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
 int mul(int a, int b){
 	return a*b;
 }
 int sub(int a, int b){
 	return a-b;
-=======
 
-=======
 int mul(int a, int b){
 	return a*b;
 }
->>>>>>> 49ff32c13b1316dbd6a8da7ca3aa1c20104270f4
 int sub(int a, int b)
 {
 	if(a>b)	return a-b;
 	else return b-a;
-<<<<<<< HEAD
->>>>>>> 44b8485d7625913061492dd5f23cc5fb73b20b5f
-=======
->>>>>>> 49ff32c13b1316dbd6a8da7ca3aa1c20104270f4
 }
