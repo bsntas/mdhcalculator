062135ddbdb671aad45db859b3d5720252241f0c
Abburu Aharneish <aharneish@gmail.com>
2023-09-23 15:53:05 +0530

    Division Function added by Abburu Aharneish and Sairaj Patro

--- functions.c
+++ functions.c
@@ -10,4 +10,16 @@ int divide (int a,int b){
 		return a/b;
 	}
 }
-
+int mul(int a, int b){
+	return a*b;
+}
+int sub(int a, int b){
+	return a-b;
+int mul(int a, int b){
+	return a*b;
+}
+int sub(int a, int b)
+{
+	if(a>b)	return a-b;
+	else return b-a;
+}
