5d24dedef59daf1a88d61f3f72e30b48bf892157
Anonymous <temp@gmail.com>
2023-09-23 16:19:20 +0530

    SSS DT

--- functions.c
+++ functions.c
@@ -38,3 +38,12 @@ int sub(int a, int b)
 	if(a>b)	return a-b;
 	else return b-a;
 }
+int factorial(int num){
+        if(num==1){
+                return 1;
+        }
+        else{
+                return num * factorial(num-1);
+        }
+}
+
